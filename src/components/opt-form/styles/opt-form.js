5e66eda38bbbb1b843942de641d0747701d03425
harshv9988 <harshv9988@gmail.com>
2021-01-03 14:06:06 +0530

    header/featue ready

--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -4,8 +4,10 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   height: 100%;
-  margin-top: 20px;
+
   flex-wrap: wrap;
+  margin: auto;
+  margin-top: 20px;
 
   @media (max-width: 1000px) {
     flex-direction: column;
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,29 @@ import React from "react";
 import { FooterContainer } from "../containers/footer";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FaqsContainer } from "../containers/faqs";
+import { HeaderContainer } from "../containers/header";
+import { Feature, OptForm } from "../components";
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Text>
+              Ready to Watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
